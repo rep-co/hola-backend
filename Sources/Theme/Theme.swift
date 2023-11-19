43bb2a4997e83eec2dbf76aed57aae40b7160880
Vadim Aleshin <mov4d@MacBook-Air-Vadim.local>
2023-11-19 14:08:41 +0300

    theme id

--- Sources/Theme/Theme.swift
+++ Sources/Theme/Theme.swift
@@ -1,13 +1,19 @@
-public struct Theme: Codable, Sendable {
+import IdentifiedCollections
+import Tagged
+
+public struct Theme: Codable, Identifiable, Sendable {
+  public let id: Tagged<Self, Int>
   public let mainEmoji: String
   public let secondaryEmoji: String
   public let tertiaryEmoji: String
 
   public init(
+    id: ID,
     mainEmoji: String,
     secondaryEmoji: String,
     tertiaryEmoji: String
   ) {
+    self.id = id
     self.mainEmoji = mainEmoji
     self.secondaryEmoji = secondaryEmoji
     self.tertiaryEmoji = tertiaryEmoji
@@ -16,36 +22,42 @@ public struct Theme: Codable, Sendable {
 
 extension Theme {
   public static let russiaITPower = Theme(
+    id: 1,
     mainEmoji: "💻",
     secondaryEmoji: "🇷🇺",
     tertiaryEmoji: "💪🏻"
   )
 
   public static let popcorn = Theme(
+    id: 2,
     mainEmoji: "🍿",
     secondaryEmoji: "🍪",
     tertiaryEmoji: "🎈"
   )
 
   public static let vacation = Theme(
+    id: 3,
     mainEmoji: "🐳",
     secondaryEmoji: "🛟",
     tertiaryEmoji: "🚢"
   )
 
   public static let happyNewYear = Theme(
+    id: 4,
     mainEmoji: "🎅🏻",
     secondaryEmoji: "🎄",
     tertiaryEmoji: "🥳"
   )
 
   public static let haloween = Theme(
+    id: 5,
     mainEmoji: "🎃",
     secondaryEmoji: "👻",
     tertiaryEmoji: "👽"
   )
 
   public static let zombie = Theme(
+    id: 6,
     mainEmoji: "🧠",
     secondaryEmoji: "🧟‍♂️",
     tertiaryEmoji: "🔪"
--- Sources/ThemeController/ThemeRouter.swift
+++ Sources/ThemeController/ThemeRouter.swift
@@ -1,16 +1,23 @@
 import CasePaths
-import Packs
+import Theme
 import VaporRouting
 
 @CasePathable
 public enum ThemeRoute {
-  case index
+    case theme(id: Theme.ID)
+    case index
 }
 
 public struct ThemeRouter: ParserPrinter {
   public var body: some VaporRouting.Router<ThemeRoute> {
     OneOf {
       Route(.case(ThemeRoute.index))
+        
+        Route(.case(ThemeRoute.theme)) {
+            Path {
+                Digits().map(.representing(Theme.ID.self))
+            }
+        }
     }
   }
 
