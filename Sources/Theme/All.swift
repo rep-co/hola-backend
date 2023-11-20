7d0cc3d41c46514bc0eff932100639988accb2e9
tplaymeow <wersa2002@gmail.com>
2023-11-20 22:38:10 +0300

    format files

--- Sources/Theme/All.swift
+++ Sources/Theme/All.swift
@@ -1,12 +1,12 @@
 import IdentifiedCollections
 
 extension Theme {
-    public static let all: IdentifiedArrayOf<Self> = [
-        .haloween,
-        .happyNewYear,
-        .popcorn,
-        .vacation,
-        .zombie,
-        .russiaITPower
-    ]
+  public static let all: IdentifiedArrayOf<Self> = [
+    .haloween,
+    .happyNewYear,
+    .popcorn,
+    .vacation,
+    .zombie,
+    .russiaITPower,
+  ]
 }
